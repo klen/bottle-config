a43d3415a5e0f4873f10fcca8e0f9c2aa6609e93
Kirill Klenov <horneds@gmail.com>
2014-12-20 16:25:17 +0300

    Bump version: 0.1.0 → 0.1.1

--- bottle_config.py
+++ bottle_config.py
@@ -3,7 +3,7 @@
 # Package information
 # ===================
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __project__ = "bottle-config"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
